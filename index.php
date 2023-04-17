886bbc3702e28cfaeeb79629eea7b1410de288dc
Victor-2691 <victor.salgado.martinez@cuc.cr>
2023-04-17 15:21:58 -0600

    ajustes

--- index.php
+++ index.php
@@ -6,56 +6,54 @@ require 'includes/config/database.php';
 $db = conectarBD();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $Usario = $_POST['usuario'];
-    $contra = $_POST['password'];
+    $Usario = $_POST['usuario'] ?? NULL;
+    $contra = $_POST['password'] ?? NULL;
 
     //  var_dump($Usario);
     //  var_dump($contra);
     //  exit; 
+    $consulta = "SELECT * FROM SA_Usuarios_Administracion WHERE correo_electronico = '$Usario'";
+    $ejecutar = mysqli_query($db, $consulta);
+    
+    if ($ejecutar->num_rows) {
+    
+        $consulta = mysqli_fetch_assoc($ejecutar);
+    
+        if ($Usario == $consulta['correo_electronico'] &&  $contra == $consulta['contrasena']) {
+    
+            session_start();
+    
+            $_SESSION['idusuario'] = $consulta['id_cliente'];
+            $_SESSION['id_rol'] = $consulta['id_rol'];
+            $_SESSION['nombre'] = $consulta['nombre'];
+            $_SESSION['apellido'] = $consulta['primer_apellido'];
+            $_SESSION['login'] = true;
+            
+            if( $_SESSION['id_rol'] == 1){
+                header("location: perfiles.php");
+            }
+    
+            if( $_SESSION['id_rol'] == 2){
+                header("location: indicadores.php");
+            }
+    
+            if( $_SESSION['id_rol'] == 3){
+                header("location: administrador.php");
+            }
+            // Llenar el arreglo de la sesion
+        } else {
+            echo  "<script>alert('Usuario o contra incorrectas');</script>";
+        }
+    } else {
+        echo  "<script>alert('Usuario no existe');</script>";
+    }
+
+
 }
 
 // Revisar si el usuario existe 
 
-$consulta = "SELECT * FROM SA_Usuarios_Administracion WHERE correo_electronico = '$Usario'";
-$ejecutar = mysqli_query($db, $consulta);
 
-if ($ejecutar->num_rows) {
-
-    $consulta = mysqli_fetch_assoc($ejecutar);
-
-    if ($Usario == $consulta['correo_electronico'] &&  $contra == $consulta['contrasena']) {
-
-        session_start();
-
-        $_SESSION['idusuario'] = $consulta['id_cliente'];
-        $_SESSION['id_rol'] = $consulta['id_rol'];
-        $_SESSION['nombre'] = $consulta['nombre'];
-        $_SESSION['apellido'] = $consulta['primer_apellido'];
-        $_SESSION['login'] = true;
-        
-        if( $_SESSION['id_rol'] == 1){
-            header("location: perfiles.php");
-        }
-
-        if( $_SESSION['id_rol'] == 2){
-            header("location: indicadores.php");
-        }
-
-        if( $_SESSION['id_rol'] == 3){
-            header("location: administrador.php");
-        }
-        // Llenar el arreglo de la sesion
-
-
-
-
-
-    } else {
-        echo  "<script>alert('Usuario o contra incorrectas');</script>";
-    }
-} else {
- 
-}
 
 ?>
 
@@ -82,7 +80,7 @@ if ($ejecutar->num_rows) {
                     </div>
                 </div>
 
-                <form method="POST" id="loginform">
+                <form method="POST" id="loginform" action="index.php">
                     <input type="text" name="usuario" placeholder="Usuario" required>
 
                     <input type="password" placeholder="Contraseña" name="password" required>
