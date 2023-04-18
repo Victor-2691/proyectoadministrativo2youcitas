23668a4e4555c279f94e1b9a16342fe953de7128
Victor-2691 <victor.salgado.martinez@cuc.cr>
2023-04-17 23:13:42 -0600

    Ajuste de header y footer

---
+++ includes/templates/header_administrador.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2 YouCitas</title>
+    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+
+<body>
+    <!-- Para solo mostrar el header en la pagina de inicio
+So es true mostramos el header inicio que tiene la imagen -->
+    <header class="header">
+        <div class="contenido-header">
+            <div class="barra">
+                <a class="" href="">
+                    <img  class="imgloco" src="build/img/logo2youcitas150final.svg" alt="Logotipo 2YouCitas">
+                </a>
+                <!-- <div class="mobile-menu">
+                <img  class="imgloco IMGLogo" src="build/img/Logo2Youcitas_Sinfondo.svg" alt="Logotipo 2YouCitas">
+                </div> -->
+
+                <div class="derecha">
+                
+                    <nav class="navegacion">
+                        <a href="index.php">Inicio</a>
+                        <a href="inicio_sesion.php">Iniciar Sesión</a>
+                        <a href="registro.php">Registrarse</a>
+                       
+                    </nav>
+                </div>
+            </div> <!--.barra-->
+        </div>
+    </header>
