e604e97b95e7489f4c8c26068774c5070a24ba2d
Victor-2691 <victor.salgado.martinez@cuc.cr>
2023-04-17 23:16:20 -0600

    Ajuste

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -6,18 +6,7 @@
         $fecha =  date('Y');
         ?>
         <p class="copyright">Todos los derechos Reservados <?php echo date('Y') ?> &copy;</p>
-        <a href="#" target="_blank">
-            <img class="footer " src="build/img/icons8-facebook.svg" alt="icono-facebook">
-        </a>
-        <a href="#" target="_blank">
-            <img class="footer " src="build/img/icons8-instagram.svg" alt="icono-telegran">
-
-        </a>
-
-        <a href="https://wa.me/50671381640?text=Hola%20escribo%20a%20la%20linea%20de%20soporte%20ya%20que%20necesito%20ayuda%20con%20el%20siguiente%20tema" target="_blank">
-            <img src="build/img/icons8-whatsapp (7).svg" alt="icono-What">
-
-        </a>
+   
     </div>
 
 </footer>
--- indicadores.php
+++ indicadores.php
@@ -1,34 +1,15 @@
 <?php
 
 session_start();
-
+require 'includes/funciones.php';
+incluirTempleate('header_indicadores');
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="build/css/app.css">
-    <title>Document</title>
-</head>
-<body>
-<h1>Perfil de indicadores</h1>    
+<main>
+    <h1>Indicadores</h1>
+</main>
 
-<footer class="footer">
-    <div class="contenedor contenedor-footer">
-        <?php
-        // Obtener fecha del servidor le pasamos el formato
-        // y miniscula solo year corto 22 y Y imprime completo 2022
-        $fecha =  date('Y');
-        ?>
-        <p class="copyright">Todos los derechos Reservados <?php echo date('Y') ?> &copy;</p>
-
-    </div>
-
-</footer>
-<script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php
+incluirTempleate('footer');
+?>
